6bef35bd2aa91786e3c74f25881cf4094e0cdd0f
GeordzNZ <geordie.king@gmail.com>
2023-05-17 01:04:21 +0100

    Create Quiz object and test serialization

--- Quizzer/Program.cs
+++ Quizzer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Transactions;
+using System.Xml.Serialization;
 
 namespace Quizzer
 {
@@ -8,8 +9,55 @@ namespace Quizzer
         {
             UIMethods.HelloWorld();
 
+            var QuizList1 = new List<Quiz>();
+            var QuizList2 = new List<Quiz>();
+
+            Quiz QuizOne = new Quiz();
+            
+            QuizOne.QuizID = 1;
+            QuizOne.QuizName = "000%%Quiz name goes here$$10";
+            QuizOne.Author = "G_NZ";
+            QuizOne.QuestionsCount = 15;
+            QuizOne.QuizFileName = QuizOne.MakeFileName(QuizOne.QuizName);
+
+            Quiz QuizTwo = new Quiz();
+            
+            QuizTwo.QuizID = 2;
+            QuizTwo.QuizName = "This is the Quiz name $$10";
+            QuizTwo.Author = "G_NZ";
+            QuizTwo.QuizFileName = QuizTwo.MakeFileName(QuizTwo.QuizName);
+
+            QuizList1.Add(QuizOne);
+            QuizList1.Add(QuizTwo);
+
+            Console.WriteLine(" -- QuizList1 -- ");
+            foreach (var q in QuizList1)
+            {
+                Console.WriteLine(q);
+            }
 
 
+            string filePath = @"C:\Users\Geordie\Documents\Dev\C-Sharp\RaketeMentoring\Projects\Module06\Data\QuizList.txt";
+            
+            // Save file with QuizList1
+            XmlSerializer xmlWriter = new XmlSerializer(typeof(List<Quiz>));
+
+            using (FileStream outputFile = File.Create(filePath))
+            {
+                xmlWriter.Serialize(outputFile, QuizList1);
+            }
+
+            // Open file and write to QuizList2
+            using (FileStream inputFile = File.OpenRead(filePath))
+            {
+                QuizList2 = xmlWriter.Deserialize(inputFile) as List<Quiz>;
+            }
+
+            Console.WriteLine(" -- QuizList2 -- ");
+            foreach (var q in QuizList2)
+            {
+                Console.WriteLine(q);
+            }
         }
     }
 }
---
+++ Quizzer/Quiz.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizzer
+{
+    public class Quiz
+    {
+        public int QuizID;
+        public string QuizName;
+        public string Author;
+        public int QuestionsCount = 0;
+        public string QuizFileName;
+
+        public string MakeFileName(string Name)
+        {
+            string tempFileName = "";
+            foreach (char c in Name)
+            {
+                tempFileName += char.IsLetterOrDigit(c) ? c : '_';
+            }
+            return tempFileName + ".txt";
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {QuizID} - {QuizName} \n {QuestionsCount} q's \n {QuizFileName}";
+        }
+    }
+}

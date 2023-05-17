17205525e94605bb5012737b06eeaa357ca668a7
GeordzNZ <geordie.king@gmail.com>
2023-05-17 23:32:32 +0100

    Implement UI for start screen and Editor / Quizzer game modes

--- Quizzer/Program.cs
+++ Quizzer/Program.cs
@@ -7,57 +7,75 @@ namespace Quizzer
     {
         static void Main(string[] args)
         {
-            UIMethods.HelloWorld();
+            char gameMode = ' ';
 
-            var QuizList1 = new List<Quiz>();
-            var QuizList2 = new List<Quiz>();
-
-            Quiz QuizOne = new Quiz();
-            
-            QuizOne.QuizID = 1;
-            QuizOne.QuizName = "000%%Quiz name goes here$$10";
-            QuizOne.Author = "G_NZ";
-            QuizOne.QuestionsCount = 15;
-            QuizOne.QuizFileName = QuizOne.MakeFileName(QuizOne.QuizName);
-
-            Quiz QuizTwo = new Quiz();
-            
-            QuizTwo.QuizID = 2;
-            QuizTwo.QuizName = "This is the Quiz name $$10";
-            QuizTwo.Author = "G_NZ";
-            QuizTwo.QuizFileName = QuizTwo.MakeFileName(QuizTwo.QuizName);
-
-            QuizList1.Add(QuizOne);
-            QuizList1.Add(QuizTwo);
-
-            Console.WriteLine(" -- QuizList1 -- ");
-            foreach (var q in QuizList1)
+            while (gameMode != 'q')
             {
-                Console.WriteLine(q);
+                UIMethods.DisplayHeader();
+                UIMethods.DisplayGameModeInstructions();
+                gameMode = UIMethods.GetGameMode();
+                
+                switch (gameMode)
+                {
+                    case 'e':
+                        UIMethods.DisplayEditorInstructions();
+
+                        break;
+                    case 'p':
+                        UIMethods.DisplayPlayerInstructions();
+                        break;
+                }
+
+                
+
             }
 
+            //Quiz QuizOne = new Quiz();
 
-            string filePath = @"C:\Users\Geordie\Documents\Dev\C-Sharp\RaketeMentoring\Projects\Module06\Data\QuizList.txt";
-            
-            // Save file with QuizList1
-            XmlSerializer xmlWriter = new XmlSerializer(typeof(List<Quiz>));
+            //QuizOne.QuizID = 1;
+            //QuizOne.QuizName = "000%%Quiz name goes here$$10";
+            //QuizOne.Author = "G_NZ";
+            //QuizOne.QuestionsCount = 15;
+            //QuizOne.QuizFileName = QuizOne.MakeFileName(QuizOne.QuizName);
 
-            using (FileStream outputFile = File.Create(filePath))
-            {
-                xmlWriter.Serialize(outputFile, QuizList1);
-            }
+            //Quiz QuizTwo = new Quiz();
 
-            // Open file and write to QuizList2
-            using (FileStream inputFile = File.OpenRead(filePath))
-            {
-                QuizList2 = xmlWriter.Deserialize(inputFile) as List<Quiz>;
-            }
+            //QuizTwo.QuizID = 2;
+            //QuizTwo.QuizName = "This is the Quiz name $$10";
+            //QuizTwo.Author = "G_NZ";
+            //QuizTwo.QuizFileName = QuizTwo.MakeFileName(QuizTwo.QuizName);
 
-            Console.WriteLine(" -- QuizList2 -- ");
-            foreach (var q in QuizList2)
-            {
-                Console.WriteLine(q);
-            }
+            //QuizList1.Add(QuizOne);
+            //QuizList1.Add(QuizTwo);
+
+            //Console.WriteLine(" -- QuizList1 -- ");
+            //foreach (var q in QuizList1)
+            //{
+            //    Console.WriteLine(q);
+            //}
+
+
+            //string filePath = @"C:\Users\Geordie\Documents\Dev\C-Sharp\RaketeMentoring\Projects\Module06\Data\QuizList.txt";
+
+            //// Save file with QuizList1
+            //XmlSerializer xmlWriter = new XmlSerializer(typeof(List<Quiz>));
+
+            //using (FileStream outputFile = File.Create(filePath))
+            //{
+            //    xmlWriter.Serialize(outputFile, QuizList1);
+            //}
+
+            //// Open file and write to QuizList2
+            //using (FileStream inputFile = File.OpenRead(filePath))
+            //{
+            //    QuizList2 = xmlWriter.Deserialize(inputFile) as List<Quiz>;
+            //}
+
+            //Console.WriteLine(" -- QuizList2 -- ");
+            //foreach (var q in QuizList2)
+            //{
+            //    Console.WriteLine(q);
+            //}
         }
     }
 }
--- Quizzer/UIMethods.cs
+++ Quizzer/UIMethods.cs
@@ -6,11 +6,94 @@ using System.Threading.Tasks;
 
 namespace Quizzer
 {
-    internal class UIMethods
+    public static class UIMethods
     {
-        public static void HelloWorld()
+        const string DIVIDER = "===================================================================";
+        const string BLANKER = "                                                                   ";
+        const int POPUP_TIME = 500;
+        public static void DisplayDivider()
         {
-            Console.WriteLine("Hello world");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(DIVIDER);
+            Console.ResetColor();
+        }
+
+        public static void DisplayBlanker(int left, int top)
+        {
+            Console.SetCursorPosition(left, top);
+            Console.Write(BLANKER);
+            Console.SetCursorPosition(0, top);
+        }
+
+
+        public static void DisplayHeader()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\t\tWELCOME TO QUIZZING QUIZZER");
+            Console.WriteLine("\t\tCome and test your knowledge");
+            Console.ResetColor();
+            DisplayDivider();
+        }
+        public static void DisplayGameModeInstructions()
+        {
+            Console.WriteLine("\t\tGame mode:");
+            Console.WriteLine("\t\t  - E = Editor  --> Create / Edit quizzes.");
+            Console.WriteLine("\t\t  - Q = Quizzer --> Test your skills.");
+            Console.WriteLine("\t\t  - X = Exit    --> Run away an hide.");
+            DisplayDivider();
+        }
+
+        public static void DisplayEditorInstructions()
+        {
+            DisplayHeader();
+            Console.WriteLine("\t\tEDITOR  Game Menu");
+            Console.WriteLine("\t\t  - C = Create a new quiz.");
+            Console.WriteLine("\t\t  - E = Edit a current quiz.");
+            Console.WriteLine("\t\t  - D = Delete a current quiz.");
+            Console.WriteLine("\t\t  - R = Return to game menu.");
+            DisplayDivider();
+        }
+
+        public static void DisplayPlayerInstructions()
+        {
+            DisplayHeader();
+            Console.WriteLine("\t\tQUIZZER  Game Menu");
+            Console.WriteLine("\t\t  - L = List Available quizes.");
+            Console.WriteLine("\t\t  - R = Return to game menu.");
+            DisplayDivider();
+        }
+        
+        // TODO: Think about how to reuse this for all menu validations.
+        public static char GetGameMode()
+        {
+            char gameModeAnswer = ' ';
+            do
+            {
+                // Get user input
+                //ConsoleKeyInfo playAgainInput = UI_Methods.GetYesNoAnswer("");
+                DisplayBlanker(16,9);
+
+                Console.Write($"\t\tChoose a game mode (E/Q/X): ");
+                ConsoleKeyInfo gameModeInput = Console.ReadKey();
+
+                char casedGameModeInput = char.ToLower(gameModeInput.KeyChar);
+
+                if ((int)casedGameModeInput == 101 || // e or E
+                    (int)casedGameModeInput == 113 || // q or Q
+                    (int)casedGameModeInput == 120)   // x or X
+                {
+                    gameModeAnswer = char.Parse(gameModeInput.KeyChar.ToString());
+                    break;
+                }
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine($"\t --> Please enter only E / Q / X ");
+                Console.ResetColor();
+                Thread.Sleep(POPUP_TIME);
+            } while (true);  // Play again loop
+
+            return gameModeAnswer;
         }
     }
 }
